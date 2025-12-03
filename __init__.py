3b34093959cac10e7a1e8fca935c40bdada87028
Cathia1 <122046225+Cathia1@users.noreply.github.com>
2025-12-03 12:14:42 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -2,9 +2,10 @@ from flask import Flask, render_template_string, render_template, jsonify
 from flask import render_template
 from flask import json
 from datetime import datetime
-from urllib.request import urlopen, Request
+from urllib.request import urlopen,
 from collections import Counter
 import sqlite3
+import requests
                                                                                                                                        
 app = Flask(__name__)  #comm                                                                                                       
 @app.route("/contact/")
