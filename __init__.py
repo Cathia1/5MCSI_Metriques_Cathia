a81e2dd858fafc2aa07e1d9afd52787d2b8fa553
Cathia1 <122046225+Cathia1@users.noreply.github.com>
2025-12-03 13:58:18 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -4,7 +4,7 @@ from datetime import datetime
 from urllib.request import urlopen, Request
 import sqlite3
                                                                                                                                        
-app = Flask(_name_)                                                                                                                  
+app = Flask(__name__)                                                                                                                  
                                                                                                                                        
 @app.route('/')
 def hello_world():
@@ -86,5 +86,5 @@ def commits_page():
     return render_template("commits.html")
 
   
-if _name_ == "_main_":
+if __name__ == "__main__":
     app.run(debug=True)
